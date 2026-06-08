72dcefc2aa9f521659710eaa0ec1e524aca6c05a
Kim-ilhyeon <kih3915@naver.com>
2026-06-08 11:03:58 +0900

    second commit develop homework-bankingProgram code 26.06.08

--- Java_basic/src/com/kh/assignment/banking/BankingController.java
+++ Java_basic/src/com/kh/assignment/banking/BankingController.java
@@ -14,20 +14,31 @@ public class BankingController {
 	
 	
 	// 메소드
+	
+	// 회원 기능
+	
+	// 1. 회원가입 
+	// [전달받은 값을 통해서 Map<String, User> user에 해당 키 값이 포함되어있는지를 containsKey() 메소드를 사용해서 확인 후
+	// 중복이 없다면 user에 id와 user를 키와 벨류 값으로 추가 후 1 반환시킴, 중복이 있다면 0반환 시킴]
 	public int insertUser(String id, User user) {
-		boolean isCheck = this.user.containsKey(id);
-		if (!isCheck) {
+		boolean checkId = this.user.containsKey(id);
+		if (!checkId) {
 			this.user.put(id, user);
 			return 1;
 		}
 		return 0;
 	}
 	
-	public User logIn(String id, String password) {
+	
+	// 2. 로그인 
+	// [전달받은 id와 pwd값을 가지고 Map<String, User> user에 해당 키 값이 포함되어있는지를 containsKey() 메소드를 사용해서 확인 후
+	// 포함되어있다면 loginUser에 전달받은 키값을 가지고 있는 User객체를 저장 하여 해당 객체의 비밀번호와 전달받은 pwd가 동일한 값인지
+	// equals() 메소드로 비교 후 동일한 경우 현재 클래스의 필드 loginedId와 loginedUser에 id와 user벨류값을 각각 저장한 후 로그인한 User객체를 반환함]
+	public User logIn(String id, String pwd) {
 		boolean checkId = user.containsKey(id);
 		if (checkId) {
 			User loginUser = user.get(id);
-			if (loginUser.getPassword().equals(password)) {
+			if (loginUser.getPassword().equals(pwd)) {
 				// 로그인 성공!
 				this.loginedId = id;
 				this.loginedUser = user.get(id);
@@ -37,6 +48,9 @@ public class BankingController {
 		return null;
 	}
 	
+	// 3. 내 정보조회 
+	// [현재 클래스 필드loginedId값을 통해서 Map<String, User> user에 해당 키값이 존재하는지를 확인 후
+	// 있다면 해당 키값을 가진 user의 벨류 값(User객체)을 반환함]
 	public User viewUser() {
 		boolean isCheck = user.containsKey(loginedId);
 		if (isCheck) {
@@ -45,6 +59,9 @@ public class BankingController {
 		return null;
 	}
 	
+	// 4. 비밀번호 변경 
+	// [현재 클래스의 필드 loginedId를 통해서 현재 로그인된 회원 객체로 접근하여 setPassword() 메소드를 통해서
+	// 전달받은 비밀번호로 변경 후 제대로 변경되었는지를 확인 후 잘 변경되었으면 1을 반환시킴, 그렇지 않으면 0을 반환시킴]
 	public int updatePwd(String newPwd) {
 		user.get(loginedId).setPassword(newPwd);
 		boolean result = user.get(loginedId).getPassword().equals(newPwd);
@@ -54,18 +71,30 @@ public class BankingController {
 		return 0;
 	}
 	
+	// 5. 회원 탈퇴(삭제) 
+	// [현재 클래스의 필드 loginedId를 가지고 현재 로그인된 회원인 Map<String, User> user에 접근하여 remove()메소드로 삭제 후
+	// 다시 loginedId를 키값으로 가지고 있는지를 확인 후 없다면 현재 클래스의 필드인 loginedId와 loginedUser의 값을 null로 변경 후 1을 반환시킴]
 	public int deleteUser() {
 		user.remove(loginedId);
-		this.loginedId = null;
-		this.loginedUser = null;
-		if (loginedId == null && loginedUser == null) {
+		boolean checkId = user.containsKey(loginedId);
+		if (!checkId) {
+			this.loginedId = null;
+			this.loginedUser = null;
 			return 1;
 		}
 		return 0;
 	}
 	
-	
 	// -----------------------
+	
+	
+	// 계좌 기능
+	
+	// 1. 계좌 개설 
+	// [현재 클래스의 loginedUser의 getAccount() 메소드를 통해서 로그인된 회원의 Account에 접근한 후 해당 Account가 null(아직 생성X)이면
+	// 생성을 하는데 100000000 ~ 999999999 (9자리)사이의 값을 랜덤으로 받아서 변수에 저장하고, 중복된 값이 Map<String , User> user에 있는지
+	// 반복문을 통해서 확인을 하고 중복값이 없다면 반복문을 탈출하여 loginedUser에 접근하여 setAccount()메소드를 통해서
+	// 현재 로그인된 회원의 계좌객체를 해당 accountNo로 생성 후 1 반환시킴, 이미 계좌 있을 경우 -1반환, 실패할 경우 0 반환시킴]
 	public int insertAccount() {
 		Account account = loginedUser.getAccount();
 		boolean isDuplicate;
@@ -77,7 +106,7 @@ public class BankingController {
 				accountNo = String.valueOf(random);
 				
 				for (User u : user.values()) {
-					if (u.getAccount() != null && u.getAccount().getAccountNo().equals(accountNo)) {
+					if (u.getAccount() != null && !(u.getAccount().getAccountNo().equals(accountNo))) {
 						isDuplicate = true;
 						break;
 					}
@@ -87,20 +116,29 @@ public class BankingController {
 			
 			loginedUser.setAccount(new Account(accountNo, 0, new ArrayList()));
 			if (loginedUser.getAccount().getAccountNo() == accountNo) {
-				return 1;
+				return 1;	// 성공
 			}
 		} else {
-			return -1;			
+			return -1;		// 이미 계좌 있음
 		}
-		return 0;
+		return 0;			// 실패
 	}
 	
+	
+	// 2. 잔액 확인 
+	// [현재 클래스의 필드인 loginedUser에 접근하여 getAccount()메소드를 사용해 로그인한 회원의 계좌객체를 변수에 저장 후
+	// 해당 계좌의 잔액을 getBalance()로 접근해서 반환시킴]
 	public int viewBalance() {
 		Account account = loginedUser.getAccount();
 		int balance = account.getBalance();
 		return balance;
 	}
 	
+	// 3. 입금 
+	// [현재 클래스의 필드인 loginedUser에 접근하여 getAccount()메소드를 사용해 로그인한 회원의 계좌객체를 변수에 저장 후
+	// 해당 계좌의 잔액을 getBalance()로 접근해서 oldBalance변수에 저장 후 setBalance()메소드로 oldBalance + money(입금금액)으로 변경 후
+	// 계좌의 잔액이 음수가 아니고, 현재 계좌의 잔액이 입금 반영이 되었는지 확인 후 Account객체의 필드인 List<Transaction>에 거래내역 추가 후
+	// 1을 반환시킴, 만약 금액이 잘못 반영되었거나 반영이 안되었다면 실패라서 다시 입금 전 금액으로 다시 변경 후 0을 반환시킴
 	public int deposit(int money) {
 		Account account = loginedUser.getAccount();
 		int oldBalance = account.getBalance();
@@ -108,49 +146,56 @@ public class BankingController {
 		
 		if (!(account.getBalance() < 0) && account.getBalance() == (oldBalance + money)) {
 			account.getHistory().add(new Transaction("입금", money, "없음"));
-			return 1;
+			return 1;	// 성공
 		}
-		account.setBalance(oldBalance);
-		return 0;
+		account.setBalance(oldBalance);	// 실패한 경우 다시 입금 전 금액으로 되돌리는 작업
+		return 0;	// 실패
 	}
 
+	// 4. 송금 
+	// [현재 클래스의 필드인 loginedUser를 통해서 로그인한 회원의 계좌에 접근하여 account변수에 저장 후 getBalance() 메소드를 통해 
+	// 송금 전 금액을 oldBalance변수에 저장, 송금 전 잔액이 송금하려는 금액보다 작은 경우에는 -1을 반환시킴(= 잔액 부족)
+	// 그게 아니라면 해당 계좌의 잔액을 setBalance()를 사용해 oldBalance-money(송금금액)으로 변경 후 금액 반영이 잘 되었는지를 확인해서
+	// Account객체의 필드인 List<Transaction>에 거래내역 추가 후 1을 반환시킴, 반영이 정상적으로 되지 않았다면 송금 전 금액으로 다시 변경 후 0을 반환시킴]
 	public int transfer(int money, String accountNo) {
 		Account account = loginedUser.getAccount();
 		int oldBalance = account.getBalance();
 		
 		if (oldBalance < money) {
-			return -1;
+			return -1;	// 잔액 부족 실패
 		}
 		account.setBalance(oldBalance - money);
 		
 		if (!(account.getBalance() < 0) && account.getBalance() == (oldBalance - money)) {
 			account.getHistory().add(new Transaction("송금", money, accountNo));
-			return 1;
+			return 1;	// 성공
 		}
-		account.setBalance(oldBalance);
-		return 0;
+		account.setBalance(oldBalance);	// 실패한 경우 다시 송금 전 금액으로 되돌리는 과정
+		return 0;		// 실패
 	}
 
-	
+	// 5. 거래내역 조회 
+	// [현재 클래스의 필드인 loginedUser에 접근하여 getAccount()메소드를 통해서 로그인한 회원의 계좌를 account 변수에 저장 후
+	// 해당 Account객체의 getHistory()를 통해서 해당 계좌의 List<Transaction>를 history 변수에 저장하여 반환시킴]
 	public List<Transaction> viewTransaction() {
 		Account account = loginedUser.getAccount();
 		List<Transaction> history = account.getHistory();
 		return history;
 	}
 	
-	
+	// 6. 계좌 삭제 
+	// [현재 클래스의 필드인 loginedUser에 접근하여 setAccount()메소드를 통해서 null로 값 변경 후
+	// 현재 로그인한 회원의 계좌의 값이 null인지 확인하여 삭제되었다면 1반환, null이 아니라면 0을 반환시킴]
 	public int deleteAccount() {		
 		loginedUser.setAccount(null);
 		Account account = loginedUser.getAccount();
 		if (account == null) {
-			return 1;
+			return 1;	// 성공
 		}
-		return 0;
+		return 0;		// 실패
 	}
 
 
-	
-	
 	// getter / setter
 	public User getLoginedUser() {
 		return this.loginedUser;
--- Java_basic/src/com/kh/assignment/banking/BankingMenu.java
+++ Java_basic/src/com/kh/assignment/banking/BankingMenu.java
@@ -115,8 +115,9 @@ public class BankingMenu {
 		}
 	}
 	
-	// 회원 기능
-	// 회원가입
+	// 회원 기능 (회원가입, 로그인, 내 정보조회, 비밀번호 수정, 회원탈퇴)
+	
+	// 1. 회원가입 (사용자 입력 : id, 비밀번호, 이름)
 	public void signUpUser() {
 		boolean repeat = true;
 		while (repeat) {
@@ -127,12 +128,16 @@ public class BankingMenu {
 			sc.nextLine();
 			System.out.print("이름 : ");
 			String name = sc.nextLine();
+			
+			// 사용자에게 입력 받은 값을 사용해 BankingController의 insertUser():int 메소드 호출
 			int result = bc.insertUser(id, new User(pwd, name));
+			// insertUser()메소드의 성공 여부를 int로 반환받아 성공할 경우 -> 1 반환받음, 실패할 경우 0 반환받음
 			if (result == 1) {
 				System.out.println("정상적으로 가입이 완료 되었습니다.");
 				return;
 			} else {
-				System.out.println("회원가입에 실패했습니다.");
+				System.out.println("아이디 또는 비밀번호가 중복 되어 회원가입에 실패했습니다.");
+				// 회원가입에 실패했을 경우 반복여부를 사용자에게 입력받아 값에 따라서 분기
 				while (true) {
 					System.out.print("다시 회원가입을 진행하시겠습니까? (y/n) : ");
 					char isContinue = sc.nextLine().toUpperCase().charAt(0);
@@ -150,15 +155,19 @@ public class BankingMenu {
 		
 	}
 	
-	// 로그인
+	// 2. 로그인 (사용자 입력 : id, 비밀번호)
 	public boolean logIn() {
 		System.out.print("아이디 : ");
 		String id = sc.next();
 		System.out.print("비밀번호(공백 없이) : ");
 		String pwd = sc.next();
 		sc.nextLine();
+		
+		// 사용자에게 입력받은 값을 통해서 BankingController의 logIn():int 메소드를 호출
+		// 성공했을 경우 -> User객체 반환받음, 실패했을 경우 -> null 반환받음, 성공/실패에 따라서 boolean값 반환함
 		User user = bc.logIn(id, pwd);
 		if (user != null) {
+			// 로그인한 User객체의 이름을 사용하기 위해서 bc.logIn()은 User객체를 반환받음
 			String userName = user.getName();
 			System.out.println(userName + "님 환영합니다!!");
 			return true;
@@ -166,15 +175,17 @@ public class BankingMenu {
 		return false;
 	}
 	
-	// 내 정보 조회
+	// 3. 내 정보 조회 (사용자 입력: x)
 	public void viewUser() {
+		// bc의 viewUser():User 메소드를 호출하여 로그인한 객체를 반환받아
+		// null이 아닌 경우에만 회원의 정보를 출력
 		User loginUser = bc.viewUser();
 		if (loginUser != null) {
 			System.out.println("조회한 회원의 이름 : " + loginUser.getName());
-			if (loginUser.getAccount() != null) {
+			if (loginUser.getAccount() != null) { // User객체의 필드 중 Accuont객체가 null인지 여부에 따라서 출력을 구분
 				Account account = loginUser.getAccount();
 				System.out.println("[[ 계좌 정보 ]]");
-				System.out.println(account);
+				System.out.println(account);	// -> Account객체에 toString()이 오버라이딩 되어있음
 				
 			} else {
 				System.out.println("----계좌 없음----");
@@ -183,18 +194,21 @@ public class BankingMenu {
 		
 	}
 	
-	// 비밀번호 수정
+	// 4. 비밀번호 수정 (사용자 입력 : 기존 비밀번호, 변경할 비밀번호)
 	public void updatePassword() {
-		int count = 0;
+		int count = 0;	// 기존 비밀번호 확인 횟수
 		while (count < 3) {
 			System.out.print("기존 비밀번호 : ");
-			String oldPwd = sc.next();
+			String inputPwd = sc.next();
 			sc.nextLine();
-			String pwd = bc.getLoginedUser().getPassword();
-			if (pwd.equals(oldPwd)) {
+			String oldPwd = bc.getLoginedUser().getPassword();
+			
+			if (inputPwd.equals(oldPwd)) {	// 기존 비밀번호가 입력한 비밀번호와 일치할 경우에 newPwd를 입력받는 분기문
 				System.out.println("변경할 비밀번호 : ");
 				String newPwd = sc.next();
 				sc.nextLine();
+				
+				// bc의 updatePwd():int 메소드를 호출하여 성공했을 경우 -> 1반환받음, 실패했을 경우 -> 0반환받음
 				int result = bc.updatePwd(newPwd);
 				if (result == 1) {
 					System.out.println("비밀번호를 정상적으로 변경하였습니다.");
@@ -202,64 +216,72 @@ public class BankingMenu {
 				} else {
 					System.out.println("비밀번호 변경 실패");
 				}
-			} else {
+			} else {	// 입력한 비밀번호와 기존비밀번호와 일치하지 않을 경우 안내문 출력 및 기존 비밀번호 확인 횟수 증가하여 기회 -1
 				System.out.println("기존 비밀번호가 일치하지 않습니다.");
 				count++;
 			}
 		}
+		// count가 3이 되어 반복문을 탈출할 경우 기존비밀번호 확인불가 안내문 출력
 		System.out.println("비밀번호를 3번 틀려서 다시 메뉴로 돌아갑니다.");
 	}
 	
-	// 회원 탈퇴
+	// 5. 회원 탈퇴 (사용자 입력 : 기존 비밀번호)
 	public void deleteUser() {
-		int count = 0;
+		int count = 0;	// 기존 비밀번호 확인 횟수
 		while (count < 3) {
 			System.out.print("기존 비밀번호 : ");
-			String oldPwd = sc.next();
+			String inputPwd = sc.next();
 			sc.nextLine();
-			String pwd = bc.getLoginedUser().getPassword();
-			if (pwd.equals(oldPwd)) {
-				int result = bc.deleteUser();
+			String oldPwd = bc.getLoginedUser().getPassword();
+			
+			if (inputPwd.equals(oldPwd)) {	// 기존 비밀번호가 입력한 비밀번호와 일치할 경우에 newPwd를 입력받는 분기문
+				int result = bc.deleteUser(); // bc의 deleteUser():int 메소드를 호출하여 성공했을 경우 -> 1반환받음, 실패했을 경우 -> 0반환받음
 				if (result == 1) {
 					System.out.println("계정이 정상적으로 삭제되었습니다.");
 					return;					
 				} else {
 					System.out.println("계정 삭제 실패");
 				}
-			} else {
+			} else {	// 입력한 비밀번호와 기존비밀번호와 일치하지 않을 경우 안내문 출력 및 기존 비밀번호 확인 횟수 증가하여 기회 -1
 				System.out.println("기존 비밀번호가 일치하지 않습니다.");
 				count++;
 			}
 		}
+		// count가 3이 되어 반복문을 탈출할 경우 기존비밀번호 확인불가 안내문 출력
 		System.out.println("비밀번호를 3번 틀려서 다시 메뉴로 돌아갑니다.");
 	}
 	
 	// ----------------
 	
-	// 계좌 기능
-	// 계좌 개설
+	// 계좌 기능 (계좌개설, 잔액조회, 입금, 송금, 거래내역 조회, 계좌 삭제)
+	
+	// 1. 계좌 개설 (사용자 입력 : x)
 	public void signUpAccount() {
+		// bc의 insertAccount():int 메소드를 통해서 계좌생성 후 결과값을 성공 -> 1,이미 계좌 있을 경우 -> -1, 실패 -> 0 반환받음
 		int result = bc.insertAccount();
-		if (result == 1) {
+
+		switch (result) {
+		case 1 : 
 			System.out.println("계좌 개설 성공");
-			return;
-		} else if (result == -1) {
+			break;
+		case -1 :
 			System.out.println("이미 계좌가 존재합니다.");
-			return;
+			break;
+		case 0 : 
+			System.out.println("계좌 개설 실패ㅠ");
 		}
-		System.out.println("계좌 개설 실패ㅠ");
 	}
 	
-	// 계좌가 있는지 여부 확인
+	// +. 계좌가 있는지 여부 확인 (아래에 기능들을 하기 전에 계좌 여부를 확인하는 메소드)
 	public boolean checkAccount() {
-		Account account = bc.getLoginedUser().getAccount();
+		Account account = bc.getLoginedUser().getAccount();	// bc의 필드에 저장되어있는 로그인된 User객체의 account에 접근하여 저장
 		if (account == null) {
 			return true;
 		}
 		return false;
 	}
 	
-	// 잔액 조회
+	// 2. 잔액 조회 (사용자 입력 : x)
 	public void viewBalance() {
 		if (checkAccount()) {
 			System.out.println("아직 계좌가 존재하지 않습니다.");
@@ -270,7 +292,7 @@ public class BankingMenu {
 		System.out.println("현재 잔액 : " + balance);
 	}
 	
-	// 입금
+	// 3. 입금 (사용자 입력 : 입금금액)
 	public void deposit() {
 		if (checkAccount()) {
 			System.out.println("아직 계좌가 존재하지 않습니다.");
@@ -281,6 +303,7 @@ public class BankingMenu {
 		int money = sc.nextInt();
 		sc.nextLine();
 		
+		// bc의 deposit():int 메소드를 통해 결과값 성공 -> 1, 실패 -> 0 반환받음
 		int result = bc.deposit(money);
 		if (result == 1) {
 			System.out.println(money + "입금 성공");
@@ -289,50 +312,69 @@ public class BankingMenu {
 		}
 	}
 	
-	// 송금
+	// 4. 송금 (사용자 입력 : 보낼 계좌번호, 송금할 금액)
 	public void transfer() {
 		if (checkAccount()) {
 			System.out.println("아직 계좌가 존재하지 않습니다.");
 			return;
 		}
 		
-		System.out.print("송금할 금액 : ");
-		int tMoney = sc.nextInt();
-		sc.nextLine();
-		System.out.print("보낼 계좌번호 : ");
-		String accountNo = sc.nextLine();
+		boolean check = true;	// 송금하는 금액이 0원 이상인지를 확인하기 위해 구분해놓은 boolean값
+		int money = 0;
 		
-		int result = bc.transfer(tMoney, accountNo);
-		if (result == 1) {
+		System.out.print("보낼 계좌번호 : ");
+		String targetAccount = sc.nextLine();
+		
+		// check를 통해서 송금하는 금액이 0원 이상이 아니라면 계속 반복
+		while (check) {
+			System.out.print("송금할 금액 : ");
+			money = sc.nextInt();
+			sc.nextLine();
+			if (money <= 0) {
+				System.out.println("1원 이상 송금이 가능합니다.");
+			} else {
+				check = false;				
+			}
+		}
+		
+		// bc의 transfer():int 메소드를 통해서 성공 -> 1, 잔액보다 송금금액이 큰 경우 -1, 실패 -> 0 반환받음
+		int result = bc.transfer(money, targetAccount);
+		
+		switch (result) {
+		case 1 : 
 			System.out.println("송금 완료");
-		} else if (result == -1) {
+			break;
+		case -1 : 
 			System.out.println("잔액 부족");
-		} else {
+			break;
+		case 0 : 
 			System.out.println("송금 실패ㅠ");
 		}
 	}
 	
 	
-	// 거래내역 조회
+	// 5. 거래내역 조회 (사용자 입력 : x)
 	public void viewTransaction() {
 		if (checkAccount()) {
 			System.out.println("아직 계좌가 존재하지 않습니다.");
 			return;
 		}
-		
 		System.out.println("[ 거래 내역 ]");
+		
+		// bc의 viewTransaction():List<Transaction> 메소드를 통해서 List<Transaction>을 반환받음
+		// List의 길이가 0인 경우에는 "거래내역 없음" 출력, 0이 아닌경우 List를 for-each로 반복하여 출력
 		List<Transaction> transactions = bc.viewTransaction();
 		if (transactions.size() == 0) {
 			System.out.println("--- 거래 내역 없음 ---");
 		} else {
 			for (Transaction tr : transactions) {
-				System.out.println(tr);
+				System.out.println(tr);	// Transaction의 toString()이 오버라이딩 되어있음
 			}			
 		}
 	}
 	
 	
-	// 계좌 삭제
+	// 6. 계좌 삭제
 	public void deleteAccount() {
 		if (checkAccount()) {
 			System.out.println("아직 계좌가 존재하지 않습니다.");
